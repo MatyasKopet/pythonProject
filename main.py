2a9c622404863909b8f4eaa3c8cac772ae4bc9ff
PCU03 <pcu03@borovice.ddmp6.cz>
2023-10-24 17:59:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from turtle import*
+shape("turtle")
+speed(10)
+color("blue")
+
+def ctverec():
+def kruh():
+    for i in range(19):
+        left(20)
+        forward(20)
+tvar = textinput("ahoj","co mam kreslit: ")
+if tvar == "ctverec":
+    ctvere
+    for i in range(4):
+        forward(403)
+        left(90)
+c()
+
+if tvar == "kruh":
+    kruh()
+
+
+
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
