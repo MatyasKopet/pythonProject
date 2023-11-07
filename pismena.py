73bbd65cc9561733980f722305a76436d153f39e
matyas <tvoje.mama@seznam.cz>
2023-11-07 18:31:00 +0100

    naprogramoval jsem Q a L

---
+++ pismena.py
@@ -0,0 +1,22 @@
+from turtle import *
+def pismeno_l():
+
+
+
+
+    forward(50)
+
+    penup()
+    right(180)
+    forward(50)
+    right(90)
+    pendown()
+    forward(100)
+    right(180)
+    penup()
+    forward(100)
+    left(90)
+    forward(50)
+
+pismeno_l()
+exitonclick()
---
+++ pismeno Q.py
@@ -0,0 +1,23 @@
+from turtle import *
+def pismeno_y():
+    left(90)
+    forward(50)
+    left(30)
+    forward(50)
+    left(180)
+    penup()
+    forward(50)
+    left(120)
+    pendown()
+    forward(50)
+
+
+
+
+
+
+
+
+
+pismeno_y()
+exitonclick()
